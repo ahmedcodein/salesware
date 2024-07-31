7b60abb52d51bfb9db118596bf2f9bb349ec38a6
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-07-31 19:47:10 +0000

    feat: create prospect_index, add its views & urls

--- sales_databases/urls.py
+++ sales_databases/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('prospects/', views.ProspectIndex.as_view(), name=('prospects')),
+    path('<int:id>/', views.prospect_detail, name='prospect_detail'),
 ]
--- sales_databases/views.py
+++ sales_databases/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-from django.views import generic
 from .models import Prospect
+from django.views import generic
+from django.shortcuts import render, get_object_or_404
 
 
 # Create your views here.
 class ProspectIndex(generic.ListView):
     queryset = Prospect.objects.all().order_by('company')
     template_name = "sales_databases/prospects_index.html"
+
+
+def prospect_detail(request, id):
+    prospect = get_object_or_404(Prospect, pk=id)
+
+    return render(
+        request,
+        "sales_databases/prospect_detail.html",
+        {"prospect": prospect}
+    )
