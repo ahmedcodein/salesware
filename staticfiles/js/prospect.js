82763e6d9f41a5ceb64d8da1f0df152418533f74
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-09-21 12:04:40 +0000

    refactore: improve prospect.js code readability

--- staticfiles/js/prospect.js
+++ staticfiles/js/prospect.js
@@ -1,156 +1,150 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    // Open prospect Detail related variables
-    const openProspectDetailModal = document.getElementById('open-prospect-detail-modal');
-    const openProspectDetailModalBody = document.getElementById('open-prospect-detail-modal-body');
-    // Open New prospect Form related variables
-    const openCreateNewProspectModal = document.getElementById('open-create-new-prospect-modal');
-    const openCreateNewProspectModalBody = document.getElementById('open-create-new-prospect-modal-body');
-    // Prospect create related variables
-    const createNewProspectBtn = document.getElementById('create-new-prospect-btn');
-    const closeCreateNewProspectModalBtn = document.getElementById('create-new-prospect-close-btn');
-    const createNewProspectCancelBtn = document.getElementById('create-new-prospect-cancel-btn');
-    // Prospect edit related variables
-    const editProspectBtn = document.getElementById('edit-prospect-btn');
-    const editedProspectForm = document.getElementById('edit-delete-prospect-form');
-    const urlProspectEdit = 'prospect_edit/';
-    const deleteProspectBtn = document.getElementById('delete-prospect-btn');
-    const closeEditDeleteProspectBtn = document.getElementById('edit-delete-prospect-close-btn');
-    const openProspectDetailModalCloseBtn = document.getElementById('open-prospect-detail-modal-close-btn');
-    // Prospect Delete related variables
-    const prospectDeleteConfirmedBtn = document.getElementById('prospect-delete-confirmed-btn');
-    const urlProspectDelete = 'prospect_delete/';
-    const confirmDeleteProspectModalBody = document.getElementById('confirm-delete-prospect-modal-body');    
-    const prospectDeleteCloseConfirmModalXBtn = document.getElementById('prospect-delete-close-confirm-modal-x-btn');
-    const prospectDeleteCloseConfirmModalBtn = document.getElementById('prospect-delete-close-confirm-modal-btn');
-
-    /* Listen to the anchor element, and observe if the user clicks on any of the prospect's name
-    to show the prospect detail*/
-    openProspectDetailModal.addEventListener('show.bs.modal', function(event) {
-        const urlProspectDetail = event.relatedTarget.getAttribute('data-url');
-        openProspectDetail(urlProspectDetail, openProspectDetailModalBody);
+document.addEventListener('DOMContentLoaded', function () {
+    // Open prospect detail modal
+    document.getElementById('open-prospect-detail-modal').addEventListener('show.bs.modal', function (event) {
+        const url = event.relatedTarget.getAttribute('data-url');
+        const data = null;
+        const openProspectDetailModalBody = document.getElementById('open-prospect-detail-modal-body');
+        const edit = false;
+        const del = false;
+        actionHandler(
+            url,
+            data,
+            openProspectDetailModalBody,
+            edit,
+            del
+        );
     });
-    /* Listen to the create new prospect btn element, and observe if the user clicks to create
-    new prospect if the user does click!*/
-    // Open the modal to display the new prospect form 
-    openCreateNewProspectModal.addEventListener('show.bs.modal', function(event) {
-        const urlNewProspectForm = event.relatedTarget.getAttribute('data-url');
-        openProspectDetail(urlNewProspectForm, openCreateNewProspectModalBody);
+    // Open create new prospect modal
+    document.getElementById('open-create-new-prospect-modal').addEventListener('show.bs.modal', function (event) {
+        const url = event.relatedTarget.getAttribute('data-url');
+        const data = null;
+        const openCreateNewProspectModalBody = document.getElementById('open-create-new-prospect-modal-body');
+        const edit = false;
+        const del = false;
+        actionHandler(
+            url,
+            data,
+            openCreateNewProspectModalBody,
+            edit,
+            del
+        );
     });
-    // Submit the post request to the database to create new prospect
-    document.getElementById('create-new-prospect-form').addEventListener('submit', function(event) {
+    // Create prospect record request
+    document.getElementById('create-new-prospect-form').addEventListener('submit', function (event) {
         event.preventDefault();
-        
         const url = event.target.action;
         const data = new FormData(event.target);
+        const openCreateNewProspectModalBody = document.getElementById('open-create-new-prospect-modal-body');
+        const createNewProspectBtn = document.getElementById('create-new-prospect-btn');
+        const createNewProspectCancelBtn = document.getElementById('create-new-prospect-cancel-btn');
+        const closeCreateNewProspectModalBtn = document.getElementById('create-new-prospect-close-btn');
+        const fourthBtn = null;
+        const edit = false;
+        const del = false;
 
-        createProspectRecord(
+        actionHandler(
             url,
             data,
             openCreateNewProspectModalBody,
             createNewProspectBtn,
             createNewProspectCancelBtn,
-            closeCreateNewProspectModalBtn
+            closeCreateNewProspectModalBtn,
+            fourthBtn,
+            edit,
+            del
         );
     });
-    // Submit the prospect edit post request to the database to update prospect data
-    editProspectBtn.addEventListener('click', function(event) {
+    // Update prospect record request
+    document.getElementById('edit-prospect-btn').addEventListener('click', function (event) {
         event.preventDefault();
+        const url = 'prospect_edit/';
+        const editedProspectForm = document.getElementById('edit-delete-prospect-form');
         const data = new FormData(editedProspectForm);
-        editProspectRecord(
-            urlProspectEdit,
+        const openProspectDetailModalBody = document.getElementById('open-prospect-detail-modal-body');
+        const editProspectBtn = document.getElementById('edit-prospect-btn');
+        const deleteProspectBtn = document.getElementById('delete-prospect-btn');
+        const closeEditDeleteProspectBtn = document.getElementById('edit-delete-prospect-close-btn');
+        const openProspectDetailModalCloseBtn = document.getElementById('open-prospect-detail-modal-close-btn');
+        const edit = true;
+        const del = false;
+        actionHandler(
+            url,
             data,
             openProspectDetailModalBody,
             editProspectBtn,
             deleteProspectBtn,
             closeEditDeleteProspectBtn,
-            openProspectDetailModalCloseBtn
+            openProspectDetailModalCloseBtn,
+            edit,
+            del
         );
     });
-    // Submit the delete post request to the database to delete prospect data
-    document.getElementById('prospect-delete-confirmed-btn').addEventListener('click', function(event) {
+    // Delete prospect record request
+    document.getElementById('prospect-delete-confirmed-btn').addEventListener('click', function (event) {
         event.preventDefault();
+        const editedProspectForm = document.getElementById('edit-delete-prospect-form');
         const data = new FormData(editedProspectForm);
-        
-        deleteProspectRecord(
-            urlProspectDelete,
+        const url = 'prospect_delete/';
+        const confirmDeleteProspectModalBody = document.getElementById('confirm-delete-prospect-modal-body');
+        const prospectDeleteConfirmedBtn = document.getElementById('prospect-delete-confirmed-btn');
+        const prospectDeleteCloseConfirmModalXBtn = document.getElementById('prospect-delete-close-confirm-modal-x-btn');
+        const prospectDeleteCloseConfirmModalBtn = document.getElementById('prospect-delete-close-confirm-modal-btn');
+        const fourthBtn = null;
+        const edit = false;
+        const del = true;
+
+        actionHandler(
+            url,
             data,
             confirmDeleteProspectModalBody,
             prospectDeleteConfirmedBtn,
             prospectDeleteCloseConfirmModalXBtn,
-            prospectDeleteCloseConfirmModalBtn
+            prospectDeleteCloseConfirmModalBtn,
+            fourthBtn,
+            edit,
+            del
         );
     });
-    /* This function handles the user request to open prospect detail
-    or the new prospect form */
-    function openProspectDetail(url, body) {
-        fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            body.innerHTML = data;
-        });
-    }
-    /* This function handles the user request to create new 
-    or delete prospect and the subsequent response to the relevant modal */
-    function createProspectRecord(url, data, body, firstBtn, secondBtn, thirdBtn) {
-        fetch(url, {
-            method: 'POST',
-            body: data,
-        })
-        .then(response => response.json())
-        .then(data => {
-            const message = data.message;
-            body.innerHTML = message;
-            firstBtn.style.display = 'none';
-            secondBtn.innerHTML = 'Close';
-            // Reset the Modal and the form to default state once closes
-            secondBtn.addEventListener('click', function() {
-                location.reload();
-            });
-            thirdBtn.addEventListener('click', function() {
-                location.reload();
-            });
-        });
-    }
-    // This function handle the prospect edit request
-    function editProspectRecord(url, data, body, firstBtn, secondBtn, thirdBtn, fourthBtn) {
-        fetch(url, {
-            method: 'POST',
-            body: data,
-        })
-        .then(response => response.json())
-        .then(data => {
-            const message = data.message;
-            
-            body.innerHTML = message;
-            firstBtn.style.display = 'none';
-            secondBtn.style.display = 'none';
-            thirdBtn.addEventListener('click', function() {
-                location.reload();
-            });
-            fourthBtn.addEventListener('click', function() {
-                location.reload();
-            });        
-        });
-    }
-    /* This function handles the user request to 
-    delete prospect and the subsequent response to the relevant modal */
-    function deleteProspectRecord(url, data, body, firstBtn, secondBtn, thirdBtn) {
-        fetch(url, {
-            method: 'POST',
-            body: data,
-        })
-        .then(response => response.json())
-        .then(data => {
-            const message = data.message;
-            body.innerHTML = message;
-            firstBtn.style.display = 'none';
-            secondBtn.addEventListener('click', function() {
-                location.reload();
-            });
-            thirdBtn.addEventListener('click', function() {
-                location.reload();
-            });
-        });
+    /** This function handles user request to: create/read/update/delete prospect record. */
+    function actionHandler(url, data, body, firstBtn, secondBtn, thirdBtn, fourthBtn, edit, del) {
+        if (data === null) {
+            fetch(url)
+                .then(response => response.text())
+                .then(data => {
+                    body.innerHTML = data;
+                });
+        } else {
+            fetch(url, {
+                    method: 'POST',
+                    body: data,
+                })
+                .then(response => response.json())
+                .then(data => {
+                    const message = data.message;
+                    body.innerHTML = message;
+                    firstBtn.style.display = 'none';
+                    if (!edit) {
+                        if (!del) {
+                            secondBtn.innerHTML = 'Close';
+                        }
+                        // Reset the Modal and the form to default state once closes
+                        secondBtn.addEventListener('click', function () {
+                            location.reload();
+                        });
+                        thirdBtn.addEventListener('click', function () {
+                            location.reload();
+                        });
+                    } else {
+                        secondBtn.style.display = 'none';
+                        // Reset the Modal and the form to default state once closes
+                        thirdBtn.addEventListener('click', function () {
+                            location.reload();
+                        });
+                        fourthBtn.addEventListener('click', function () {
+                            location.reload();
+                        });
+                    }
+                });
+        }
     }
 });
