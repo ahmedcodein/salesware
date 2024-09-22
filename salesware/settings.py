9a30ebe06d89ef34a32e1a1913e462ed74552f49
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-09-22 22:02:21 +0000

    build: remove  am i responsive to allowed host & trust

--- salesware/settings.py
+++ salesware/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.codeinstitute-ide.net', '.herokuapp.com', 'https://ui.dev/amiresponsive']
+ALLOWED_HOSTS = ['.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
@@ -108,7 +108,6 @@ else:
 CSRF_TRUSTED_ORIGINS = [
     "https://*.herokuapp.com",
     "https://*.codeinstitute-ide.net",
-    "https://ui.dev/amiresponsive",
 ]
 
 
